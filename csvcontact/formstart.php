ba713e6273d00855b4910a08315cd381a3350300
EinSteini <fanokopterhd@gmail.com>
2020-07-08 11:51:32 +0200

    POST request works now

--- csvcontact/formstart.php
+++ csvcontact/formstart.php
@@ -1,4 +1,4 @@
 <div id="maindiv">
-    <form action="" method="post" >
-    <?php wp_nonce_field('csv_submit','csv_nonce'); ?>
+    <form action="wp-admin/admin-post.php" method="post" >
+    <?php //wp_nonce_field('csv_submit','csv_nonce'); ?>
     <input type="hidden" name="action" value="csv_submit">
--- morewidgets.php
+++ morewidgets.php
@@ -42,24 +42,41 @@ function csvcontact( $attr ){
     return $output;
 }
 
-add_action( 'wp_post_nopriv_csv_submit', 'write_to_csv' );
-add_action( 'wp_post_csv_submit', 'write_to_csv' );
-add_action( 'wp_post', 'write_to_csv');
-add_action( 'wp_post_nopriv', 'write_to_csv');
+add_action( 'admin_post_csv_submit', 'write_to_csv' );
+add_action( 'admin_post_nopriv_csv_submit', 'write_to_csv' );
 
 function write_to_csv(){
-    echo "<p>sflkflkksn0</p>";
-    if ( empty($_POST) || !wp_verify_nonce($_POST['csv_nonce'],'csv_submit') ) {
-        echo '<p>You targeted the right function, but sorry, your nonce did not verify.</p>';
-        die();
-    } else {
-        echo "<p>works</p>";
+    echo $_POST["csvemail"];
+    //if ( empty($_POST) || !wp_verify_nonce($_POST['csv_nonce'],'csv_submit') ) {
+      //  echo '<p>You targeted the right function, but sorry, your nonce did not verify.</p>';
+      //  die();
+    //}
+    
+    $url = wp_nonce_url('themes.php?page=example','example-theme-options');
+
+    if (false === ($creds = request_filesystem_credentials($url, '', false, false, null) ) ) {
+	    return; // stop processing here
     }
+
+    if ( ! WP_Filesystem($creds) ) {
+        request_filesystem_credentials($url, '', true, false, null);
+        return;
+    }
+
+    global $wp_filesystem;
+
+    //$wp_filesystem->put_contents(
+       // wp_plugins_dir()."MoreWidgets/csvcontact/csv/example.txt",
+      //  'Example',
+    //    FS_CHMOD_FILE
+    //);
+
+    wp_redirect( "/" );
 }
 
 add_action("wp_enqueue_scripts","load_scripts");
 function load_scripts(){
     wp_enqueue_style("bootstrap", "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css");
-    wp_enqueue_script("jscsvcontact",plugins_url("csvcontact/script.js", __FILE__));
+   // wp_enqueue_script("jscsvcontact",plugins_url("csvcontact/script.js", __FILE__));
 
 }
